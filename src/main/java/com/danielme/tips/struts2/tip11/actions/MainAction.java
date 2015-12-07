bd9fd15a2a653c273fcf73cc0598ef82cc5e8784
Daniel Medina <danielme_com@yahoo.com>
2015-12-07 21:35:18 +0100

    print language

--- src/main/java/com/danielme/tips/struts2/tip11/actions/MainAction.java
+++ src/main/java/com/danielme/tips/struts2/tip11/actions/MainAction.java
@@ -1,5 +1,7 @@
 package com.danielme.tips.struts2.tip11.actions;
 
+import org.apache.log4j.Logger;
+
 import com.opensymphony.xwork2.ActionSupport;
 
 /**
@@ -11,9 +13,12 @@ public class MainAction extends ActionSupport
 {
 	private static final long serialVersionUID = 33466065079709970L;
 	
+	private static final Logger LOG = Logger.getLogger(MainAction.class);
+	
 	public String execute() throws Exception
-	{	
+	{			
+		LOG.info("language: " + getLocale().getLanguage());
 		return SUCCESS;
-	}	
+	}		
 
 }
